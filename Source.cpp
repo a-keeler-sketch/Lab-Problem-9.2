c5f79bfee7624d45b6477e5961079b943ba8b1f7
Anna Keeler <a-keeler@onu.edu>
2025-10-30 13:47:12 -0400

    Completed isSortedIncreasing

--- Source.cpp
+++ Source.cpp
@@ -12,10 +12,34 @@
 using namespace std;
 
 
+//prototypes
+bool isSortedIncreasing(int values[], int size);
+bool isSortedDecreasing(int values[], int size);
+bool hasAdjecentDuplicates(int values[], int size);
+bool hasDuplicates(int values[], int size);
 
 int main(void) {
-   
-     
+    const int SIZE_OF_ARRAY =  6 ;
+    int array[SIZE_OF_ARRAY];
+    
+    //gets input from user
+    for (int i = 0; i < SIZE_OF_ARRAY; i++) {
+        array[i] = 0;
+        cout << "Enter integer # " << i << ": ";
+        cin >> array[i];
+
+    }
+
+    if (isSortedIncreasing(array, SIZE_OF_ARRAY) == true) cout << "The data are increasing";
+    else cout << "The data are not increasing";
+  
+
+    
+
+
+
+
+
 
 
 
@@ -23,3 +47,11 @@ int main(void) {
 
 }
 
+bool isSortedIncreasing(int values[], int size) {
+ 
+    for (int i = 1; i < size; i++) {
+        if (values[i] < values[i - 1]) return false;
+       
+    }
+    return true;
+}
